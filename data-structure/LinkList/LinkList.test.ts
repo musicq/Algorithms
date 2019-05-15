4a9ed2d45228f7fed419f97d95a81333b8ee101e
musicq <liang.kui@hotmail.com>
2019-05-15 09:43:01 +0800

    test: add testing for linklist

--- data-structure/LinkList/LinkList.test.ts
+++ data-structure/LinkList/LinkList.test.ts
@@ -1,38 +1,42 @@
 import LinkList from './LinkList';
 
-test('test empty array', () => {
-  const list = LinkList.from([]);
+describe('test tranform an given array to LinkList', () => {
+  test('test empty array', () => {
+    const list = LinkList.from([]);
 
-  expect(list).toEqual({ value: undefined, next: undefined });
+    expect(list).toEqual({ value: undefined, next: undefined });
+  });
+
+  test('test 1 length array', () => {
+    const list = LinkList.from([1]);
+
+    expect(list).toEqual({ value: 1, next: undefined });
+  });
+
+  test('test 10 length array', () => {
+    const arr = Array.from({ length: 10 }).map((_, i) => i);
+    const list = LinkList.from(arr);
+
+    let a = list;
+    let i = 0;
+
+    while (a) {
+      expect(a.value).toBe(arr[i]);
+      i++;
+      // @ts-ignore
+      a = a.next;
+    }
+  });
 });
 
-test('test 1 length array', () => {
-  const list = LinkList.from([1]);
+describe('test push method of LinkList', () => {
+  test('test push an element into a linklist head', () => {
+    const arr = Array.from({ length: 10 }).map((_, i) => i);
+    const list = LinkList.from(arr);
 
-  expect(list).toEqual({ value: 1, next: undefined });
-});
+    const pushElement = -1;
+    const newList = LinkList.push(list, pushElement);
 
-test('test 10 length array', () => {
-  const arr = Array.from({ length: 10 }).map((_, i) => i);
-  const list = LinkList.from(arr);
-
-  let a = list;
-  let i = 0;
-
-  while (a) {
-    expect(a.value).toBe(arr[i]);
-    i++;
-    // @ts-ignore
-    a = a.next;
-  }
-});
-
-test('test push an element into a linklist', () => {
-  const arr = Array.from({ length: 10 }).map((_, i) => i);
-  const list = LinkList.from(arr);
-
-  const pushElement = -1;
-  const newList = LinkList.push(list, pushElement);
-
-  expect(newList.value).toBe(pushElement);
+    expect(newList.value).toBe(pushElement);
+  });
 });
