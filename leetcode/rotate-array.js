47743650da575504f568492b5e5b71060b6cac0c
Liang, Kui <kui.liang@sc.com>
2019-11-22 09:51:33 +0800

    imporve #189

--- leetcode/rotate-array.js
+++ leetcode/rotate-array.js
@@ -4,14 +4,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    if (k === 0) {
-        return nums;
+    k = k % nums.length;
+    const reverse = (list, start, end) => {
+        while(start < end) {
+            const t = list[start];
+            list[start] = list[end];
+            list[end] = t;
+            start++;
+            end--;
+        }
     }
-    
-    for (let i = 0; i < k; i++) {
-        const last = nums.pop();
-        nums.unshift(last);
-    }
-    
-    return nums;
+
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length -1);
 };
+
