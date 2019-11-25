3be32cdf7750d621cd3babee6dbd13267ae8c741
Liang, Kui <kui.liang@sc.com>
2019-11-25 09:34:38 +0800

    imporve #88

--- leetcode/merge-sorted-array.js
+++ leetcode/merge-sorted-array.js
@@ -6,46 +6,21 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    let needSort = false;
-    let i = j = 0;
-    let lastIdx = 0;
+    let i = m - 1;
+    let j = n - 1;
+    let l = m + n -1;
 
-    while (i < m + n) {
-        if (nums1[i] > nums2[j] && i < m) {
-            nums1[m] = nums2[j];
-            m += 1;
-            j += 1;
-            n -= 1;
-            lastIdx += 1;
-            needSort = true;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[l--] = nums2[i--];
         } else {
-            if (needSort) {
-                reverse(nums1, i, m-1);
-                reverse(nums1, i, i + lastIdx - 1)
-                reverse(nums1, i + lastIdx, m-1);
-
-                lastIdx = 0;
-                needSort = false;
-            }
-
-            if (i >= m) {
-                nums1[i] = nums2[j];
-                j += 1;
-            }
-
-            i += 1;
+            nums1[l--] = nums2[j--];
         }
+    }
+
+    while (j >= 0) {
+        nums1[l--] = nums2[j--];
     }
 
     return nums1;
 };
-
-function reverse(list, start, end) {
-    while(start < end) {
-        const t = list[start];
-        list[start] = list[end];
-        list[end] = t;
-        start += 1;
-        end -= 1;
-    }
-}
