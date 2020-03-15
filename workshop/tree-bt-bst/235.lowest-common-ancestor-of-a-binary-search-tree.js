ca932f50695ffccf3b94af2039e6a34999d41111
Liang, Kui <liang.kui@hotmail.com>
2020-03-15 14:03:50 +0800

    235

---
+++ workshop/tree-bt-bst/235.lowest-common-ancestor-of-a-binary-search-tree.js
@@ -0,0 +1,98 @@
+/*
+ * @lc app=leetcode id=235 lang=javascript
+ *
+ * [235] Lowest Common Ancestor of a Binary Search Tree
+ *
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (48.14%)
+ * Likes:    1661
+ * Dislikes: 95
+ * Total Accepted:    363.2K
+ * Total Submissions: 753.8K
+ * Testcase Example:  '[6,2,8,0,4,7,9,null,null,3,5]\n2\n8'
+ *
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of
+ * two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor
+ * is defined between two nodes p and q as the lowest node in T that has both p
+ * and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant
+ * of itself according to the LCA definition.
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * All of the nodes' values will be unique.
+ * p and q are different and both values will exist in the BST.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if (root === null) return root;
+
+  while (root) {
+    if (root.val < p.val && root.val < q.val) {
+      root = root.right;
+    } else if (root.val > p.val && root.val > q.val) {
+      root = root.left;
+    } else {
+      return root;
+    }
+  }
+
+  return null;
+};
+// @lc code=end
+
+// recursive
+var lowestCommonAncestor = function(root, p, q) {
+  if (root === null) return root;
+
+  if (root.val < p.val && root.val < q.val) {
+    return lowestCommonAncestor(root.right, p, q);
+  }
+  if (root.val > p.val && root.val > q.val) {
+    return lowestCommonAncestor(root.left, p, q);
+  }
+
+  return root;
+};
