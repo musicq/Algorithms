4d0cf7de2a23f93548ea6e28fb07843e42e104dc
Liang, Kui <liang.kui@hotmail.com>
2020-03-26 14:30:08 +0800

    42

--- workshop/stack-quequ-deque/42.trapping-rain-water.js
+++ workshop/stack-quequ-deque/42.trapping-rain-water.js
@@ -15,18 +15,18 @@
  *
  * Given n non-negative integers representing an elevation map where the width
  * of each bar is 1, compute how much water it is able to trap after raining.
- * 
- * 
+ *
+ *
  * The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
  * In this case, 6 units of rain water (blue section) are being trapped. Thanks
  * Marcos for contributing this image!
- * 
+ *
  * Example:
- * 
- * 
+ *
+ *
  * Input: [0,1,0,2,1,0,1,3,2,1,2,1]
  * Output: 6
- * 
+ *
  */
 
 // @lc code=start
@@ -35,7 +35,26 @@
  * @return {number}
  */
 var trap = function(height) {
-    
+  if (!height || height.length === 0) return 0;
+
+  let s = [];
+  let ans = 0;
+
+  for (let i = 0; i < height.length; i++) {
+    while (s.length > 0 && height[i] > height[s[s.length - 1]]) {
+      let top = s.pop();
+
+      if (s.length === 0) break;
+
+      let peek = s[s.length - 1];
+
+      let w = i - peek - 1;
+      ans += w * (Math.min(height[i], height[peek]) - height[top]);
+    }
+
+    s.push(i);
+  }
+
+  return ans;
 };
 // @lc code=end
-
