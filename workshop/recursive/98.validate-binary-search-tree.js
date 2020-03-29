d4afaa56431437a101eca92f5cf8e90e61da0c7d
Liang, Kui <liang.kui@hotmail.com>
2020-03-29 11:26:07 +0800

    98

---
+++ workshop/recursive/98.validate-binary-search-tree.js
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=98 lang=javascript
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (27.16%)
+ * Likes:    3237
+ * Dislikes: 459
+ * Total Accepted:    604.2K
+ * Total Submissions: 2.2M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ *
+ * Assume a BST is defined as follows:
+ *
+ *
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ *
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ *
+ * Input: [2,1,3]
+ * Output: true
+ *
+ *
+ * Example 2:
+ *
+ *
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ *
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+  return isValid(root, Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY);
+};
+
+function isValid(root, min, max) {
+  if (root === null) return true;
+
+  if (min >= root.val || root.val >= max) return false;
+
+  return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
+}
+// @lc code=end
