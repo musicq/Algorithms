1500bcd8225eebf952f0b4c0f84736eaa9c16aa5
Liang, Kui <liang.kui@hotmail.com>
2020-03-29 23:14:04 +0800

    manage directory

--- workshop/tree-bt-bst/144.binary-tree-preorder-traversal.js
+++ workshop/tree-bt-bst/144.binary-tree-preorder-traversal.js
@@ -14,22 +14,22 @@
  * Testcase Example:  '[1,null,2,3]'
  *
  * Given a binary tree, return the preorder traversal of its nodes' values.
- *
+ * 
  * Example:
- *
- *
+ * 
+ * 
  * Input: [1,null,2,3]
  * ⁠  1
  * ⁠   \
  * ⁠    2
  * ⁠   /
  * ⁠  3
- *
+ * 
  * Output: [1,2,3]
- *
- *
+ * 
+ * 
  * Follow up: Recursive solution is trivial, could you do it iteratively?
- *
+ * 
  */
 
 // @lc code=start
@@ -47,11 +47,31 @@
 var preorderTraversal = function(root) {
   if (root === null) return [];
 
-  let s = [root];
   let r = [];
+  traversal(root, r);
+
+  return r;
+};
+
+function traversal(root, r) {
+  if (root === null) return;
+
+  r.push(root.val);
+
+  traversal(root.left, r);
+  traversal(root.right, r);
+}
+// @lc code=end
+
+var preorderTraversal = function(root) {
+  if (root === null) return [];
+  
+  let r = [];
+  let s = [root];
 
   while (s.length !== 0) {
     root = s.pop();
+
     r.push(root.val);
 
     if (root.right) s.push(root.right);
@@ -60,4 +80,3 @@ var preorderTraversal = function(root) {
 
   return r;
 };
-// @lc code=end
