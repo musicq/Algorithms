e806fc6e197b36be3b9eeb41d605dc2e07cdc6ef
Liang, Kui <liang.kui@hotmail.com>
2020-08-16 18:28:36 +0800

    283

--- workshop/array-linkedlist-skiplist/283.move-zeroes.js
+++ workshop/array-linkedlist-skiplist/283.move-zeroes.js
@@ -9,36 +9,38 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-  if (!nums || nums.length === 0) return;
+var moveZeroes = function (nums) {
+  if (!nums || nums.length === 0) return
 
-  let j = 0;
+  let j = 0
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      nums[j] = nums[i];
+      nums[j] = nums[i]
       if (i !== j) {
-        nums[i] = 0;
+        nums[i] = 0
       }
-      j++;
+      j++
     }
   }
-};
+}
 // @lc code=end
 
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-  let j = 0;
+var moveZeroes = function (nums) {
+  if (!nums || nums.length < 2) return
+
+  let j = 0
 
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
-      let t = nums[j];
-      nums[j] = nums[i];
-      nums[i] = t;
+      let t = nums[j]
+      nums[j] = nums[i]
+      nums[i] = t
 
-      j++;
+      j++
     }
   }
-};
+}
